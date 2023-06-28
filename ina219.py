a7b5ce2c3c0d0f72745007d9a79ea51754bb5b3a
Erwanweb <44497874+Erwanweb@users.noreply.github.com>
2023-06-28 17:03:48 +0200

    Update ina219.py

--- ina219.py
+++ ina219.py
@@ -58,7 +58,7 @@ class Mode:
 
 
 class INA219:
-    def __init__(self, i2c_bus=1, addr=0x40): # Pi - i2c_bus=1 - c4 i2c_bus=0
+    def __init__(self, i2c_bus=0, addr=0x40): # Pi - i2c_bus=1 - c4 i2c_bus=0
         #self.bus = smbus.SMBus(i2c_bus) #pour smsbus (v1) lib
         self.bus = SMBus(i2c_bus)
         self.addr = addr
